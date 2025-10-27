f5db21a21a9044a61411a33d30e5c07a074729ed
gabby <hannahfrench1709@hotmail.com>
2025-10-27 12:12:32 +0000

    feat: set up basic home page feature

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,30 @@
-export default function HomePage () {
+"use client";
+
+import { useMemo } from "react";
+import dynamic from "next/dynamic";
+
+export default function HomePage() {
+  const Map = useMemo(
+    () =>
+      dynamic(() => import("@/components/Map"), {
+        loading: () => <p>A map is loading</p>,
+        ssr: false,
+      }),
+    []
+  );
+
+  // const placeQuery = db.query(
+  //   `SELECT endpoint, `
+  // )
+
   return (
-    <>
-      <h1>Home Page</h1>
-    </>
-  )
+    <div>
+      <h1>Welcome to Spooky Spots!</h1>
+
+      <Map width={50} height={50} />
+
+      <h2>Browse our lists of Spooky Spots</h2>
+
+    </div>
+  );
 }
