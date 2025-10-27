d16f1362283bce9e6fbf956ac07106d53d3a7b07
Sparkes7 <willsparkes93@gmail.com>
2025-10-27 11:34:36 +0000

    add place marker on map feature

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,27 +1,62 @@
-import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet";
+import {
+  MapContainer,
+  Marker,
+  TileLayer,
+  Popup,
+  useMapEvent,
+  useMap,
+} from "react-leaflet";
+import { useState } from "react";
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
+import Link from "next/link";
 
 export default function MyMap(props) {
   const { position, zoom } = props;
 
-  return (
-    <MapContainer
-      center={{ lat: 51.505, lng: -0.09 }}
-      zoom={19}
-      style={{ height: "500px", width: "100%" }}
-      scrollWheelZoom={true}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={{ lat: 51.505, lng: -0.09 }}>
+  console.log(props);
+
+  function PlaceMarker() {
+    const [position, setPosition] = useState(null);
+
+    useMapEvent("click", (e) => {
+      setPosition(e.latlng);
+    });
+
+    return position === null ? null : (
+      <Marker position={position}>
         <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
+          <div className="flex flex-col text-center">
+            <Link href="/">Home</Link>
+            <p>
+              Lat: {position.lat} <br /> Lng: {position.lng}
+            </p>
+          </div>
         </Popup>
       </Marker>
-    </MapContainer>
+    );
+  }
+
+  return (
+    <div className={`w-[${props.width}]% h-[${props.height}]vh`}>
+      <MapContainer
+        center={{ lat: 53.5, lng: -1.509 }}
+        zoom={8}
+        style={{ height: `${props.height}vh`, width: `${props.width}%` }}
+        scrollWheelZoom={true}
+      >
+        <PlaceMarker />
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={{ lat: 51.505, lng: -0.09 }}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   );
 }
