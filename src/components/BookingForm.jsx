0955fdfa0a53a54e23a5d802981cea227429dd40
Sparkes7 <willsparkes93@gmail.com>
2025-10-28 09:12:38 +0000

    created a booking form component and added it to place page

---
+++ src/components/BookingForm.jsx
@@ -0,0 +1,67 @@
+"use client";
+import { useState } from "react";
+
+export default function BookingForm({ slots }) {
+  const [groupSize, setGroupSize] = useState(0);
+  const [date, setDate] = useState(null);
+
+  function handleBooking(formData) {
+    const formDate = formData.get("date");
+    //console.log(Date.parse(formDate));
+    const formValues = {
+      date: formData.get("date"),
+      groupsize: formData.get("groupsize"),
+    };
+    console.log(formValues);
+  }
+
+  return (
+    <>
+      <section>
+        <h1>Booking Form</h1>
+        <form action={handleBooking} className="flex">
+          <div>
+            <label htmlFor="date">Pick a date:</label>
+            <input
+              type="date"
+              name="date"
+              defaultValue={date}
+              onChange={(e) => {
+                setDate(e.target.value);
+              }}
+            />
+          </div>
+          <div>
+            <button
+              onClick={() => {
+                if (groupSize > 0) {
+                  setGroupSize(groupSize - 1);
+                }
+              }}
+            >
+              -
+            </button>
+
+            <input type="text" name="groupsize" value={groupSize} />
+
+            <button
+              onClick={() => {
+                if (groupSize < slots) {
+                  setGroupSize(groupSize + 1);
+                }
+              }}
+            >
+              +
+            </button>
+          </div>
+          <button
+            type="submit"
+            className="bg-zinc-500 p-1 border border-1-zinc-200"
+          >
+            Confirm Booking
+          </button>
+        </form>
+      </section>
+    </>
+  );
+}
