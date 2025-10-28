9a1818b3b906404075b35b5dbf983f5ac6366e79
Sparkes7 <willsparkes93@gmail.com>
2025-10-28 11:03:06 +0000

    added manage place page with validation for the owners username

---
+++ src/app/[place]/manage/page.js
@@ -0,0 +1,26 @@
+import { db } from "@/utils/dbConnection";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+export default async function ManagePlace({ params }) {
+  const placeParam = (await params).place;
+  console.log(placeParam);
+
+  const user = await currentUser();
+  if (!user) {
+    redirect(`/${placeParam}`);
+  }
+
+  const placeData = await db.query(`SELECT * FROM places WHERE endpoint = $1`, [
+    placeParam,
+  ]);
+  if (user.username !== placeData.rows[0].owner_username) {
+    redirect(`/${placeParam}`);
+  }
+
+  return (
+    <div>
+      <p>Manage Place Page</p>
+    </div>
+  );
+}
