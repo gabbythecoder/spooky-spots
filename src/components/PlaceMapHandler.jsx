fe645bef6fae25496440c8183126d437775a0f57
Sparkes7 <willsparkes93@gmail.com>
2025-10-30 10:46:00 +0000

    add validation to check the logged in user is an admin to access the new place page

---
+++ src/components/PlaceMapHandler.jsx
@@ -0,0 +1,22 @@
+"use client";
+
+import "leaflet/dist/leaflet.css";
+import { useMemo } from "react";
+import dynamic from "next/dynamic";
+
+export default function NewPlaceHandler() {
+  const Map = useMemo(
+    () =>
+      dynamic(() => import("@/components/NewPlaceMap"), {
+        loading: () => <p>A map is loading</p>,
+        ssr: false,
+      }),
+    []
+  );
+
+  return (
+    <>
+      <Map width={50} height={50} />
+    </>
+  );
+}
